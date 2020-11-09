5216c92931473686d7dba26f6cb0b96b8b7a5ae6
Edith Werbel <edith.werbel@gmail.com>
2020-11-09 12:47:52 -0500

    forms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { MediaItemComponent } from './media-item.component';
 import { MediaItemListComponent } from './media-item-list.component';
@@ -9,20 +9,20 @@ import { CategoryListPipe } from './category-list.pipe';
 import { MediaItemFormComponent } from './media-item-form.component';
 
 @NgModule({
-  imports: [
-    BrowserModule,
-    FormsModule
-  ],
-  declarations: [
-    AppComponent,
-    MediaItemComponent,
-    MediaItemListComponent,
-    FavoriteDirective,
-    CategoryListPipe,
-    MediaItemFormComponent
-  ],
-  bootstrap: [
-    AppComponent
-  ]
+    imports: [
+        BrowserModule,
+        ReactiveFormsModule
+    ],
+    declarations: [
+        AppComponent,
+        MediaItemComponent,
+        MediaItemListComponent,
+        FavoriteDirective,
+        CategoryListPipe,
+        MediaItemFormComponent
+    ],
+    bootstrap: [
+        AppComponent
+    ]
 })
-export class AppModule {}
+export class AppModule { }
